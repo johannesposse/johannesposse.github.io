8d43340435ba98fabd23d008170c060f1ea0b8be
Johannes Posse <johannesposse@gmail.com>
2021-05-31 20:52:19 +0200

    changes

---
+++ LetsGoViking/main.js
@@ -0,0 +1,18 @@
+let menuIco = document.querySelector("#menuIco");
+let mobileMenu = document.querySelector(".mobileMenu");
+mobileMenu.style.display = "none";
+
+menuIco.addEventListener("click", function(){
+    
+
+    if(mobileMenu.style.display === "none"){
+        mobileMenu.style.display = "flex"
+        mobileMenu.style.webkitAnimationName = "slide-right";
+        mobileMenu.style.webkitAnimationDuration =".5s";
+
+    }else{
+        mobileMenu.style.webkitAnimationName = "slide-left";
+        mobileMenu.style.webkitAnimationDuration =".5s";
+        mobileMenu.style.display = "none"
+    }
+})
