ec1403fc3961134b6ba2c62d0ebbef7745b1c53a
Johannes Posse <johannesposse@gmail.com>
2021-06-01 21:15:04 +0200

    asdasd

--- LetsGoViking/main.js
+++ LetsGoViking/main.js
@@ -28,36 +28,41 @@ function sendEmail(){
     let lastName = document.querySelector("#lastName");
     let messages = document.querySelector("#message");
 
-    Email.send({
-        Host : "mail1.webnode.com",
-        Username : "info@letsgoviking.com",
-        Password : "xxxx",
-        To : 'info@letsgoviking.com',
-        From : "info@letsgoviking.com",
-        Subject : "Nytt meddelande från Let's Go Viking",
-        Body : "Förnamn: " + firstName.value + "<br>" + "Efternamn: " + lastName.value + "<br>" + "Epost: " + email.value + "<br>" +"Meddelande:"+ messages.value
-    }).then(
-        message => alert(message)
-      );
-    // .then(
-    // message => document.querySelector("#pConfirm").innerText = message,
-    // confirm()
-    // //greenyellow
-    // );
+    if(email.value == "" || firstName.value == "" || lastName.value == "" || messages.value == ""){
+        document.querySelector("#pConfirm").innerText = "Vänligen fyll i all info"
+        document.querySelector(".confirm").style.display ="block",
+        document.querySelector(".confirm").style.webkitAnimationName = "slide-right",
+        document.querySelector(".confirm").style.webkitAnimationDuration =".5s"
+        document.querySelector(".confirm").style.backgroundColor ="red"
+    }else{
+        Email.send({
+            SecureToken : "9f204852-7f1d-48f9-926e-ae61e3b475fa",
+            To : 'info@letsgoviking.com',
+            From : "info@letsgoviking.com",
+            Subject : "Nytt meddelande från Let's Go Viking",
+            Body : "Förnamn: " + firstName.value + "<br>" + "Efternamn: " + lastName.value + "<br>" + "Epost: " + email.value + "<br>" +"Meddelande:"+ messages.value
+        }).then(
+        message => document.querySelector("#pConfirm").innerText = message,
+        confirm()
+        );
+    }
+
 
 
 }
 
 function confirm()
 {
-    // if(message => document.querySelector("#pConfirm").innerText == "OK"){
-    //     document.querySelector(".btnSendMail").style.backgroundColor ="greenyellow"
-    // }else{
-    //     document.querySelector(".btnSendMail").style.backgroundColor ="red"
-    // }
+    if(message => document.querySelector("#pConfirm").innerText == "OK"){
+        document.querySelector(".confirm").style.backgroundColor ="greenyellow"
+    }else{
+        document.querySelector(".confirm").style.backgroundColor ="red"
+    }
 
+    document.querySelector(".kontakt").style.display ="none",
+    document.querySelector("#pConfirm").innerText = "Tack, ditt mejl är skickat"
     document.querySelector(".confirm").style.display ="block",
     document.querySelector(".confirm").style.webkitAnimationName = "slide-right",
     document.querySelector(".confirm").style.webkitAnimationDuration =".5s"
-
+   
 }
